44eb9074063308c625efc89562e0d0c6b3f4f379
brufolharini <bruno@bftecno.com.br>
2023-10-18 22:12:11 -0300

    primeiro projeto sozinho

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import random
 
 # Desafio que eu mesmo resolvi criar.
-numero = input(
-    'Será que você consegue acertar o número que o Computador está pensando? Digite um número entre 8 a 20: ')
+numero = input('Será que você consegue acertar o número que o Computador está pensando? Digite um número entre 8 a 20: ')
 print(numero)
 print(random.randint(8, 20))
